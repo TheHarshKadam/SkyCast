3d09eb11a29f9398a30520dbc9fc49a41868216d
TheHarshKadam <harshvardhankadam89@gmail.com>
2025-08-25 22:51:52 +0530

    updated app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Weather from './weather'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-      <div className='text-3xl font-bold underline'>
-        Hello World!
-      </div>
+    <div>
+      <Weather/>
+    </div>
   )
 }
 
